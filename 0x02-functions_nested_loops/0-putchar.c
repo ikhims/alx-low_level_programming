b60d094763dbb16f188ba909734fb9ee1be489d8
IK <ikediae1@gmail.com>
2022-03-15 22:52:52 +0100

    0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/* more headers goes there */
+
+/**
+* main - entry point
+*
+* Return: 0 if success
+*/
+int main()
+{
+	/* your code goes there */
+	printf("_putchar\n");
+	return (0);
+}
--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 The repository for C programming task in ALX
+A readme file
