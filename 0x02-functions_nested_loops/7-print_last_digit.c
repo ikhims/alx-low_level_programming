04d3d1ac4e616c621a5015f04775fff6a8dfd30e
IK <ikediae1@gmail.com>
2022-03-16 15:19:52 +0100

    7-print_last_digit.c

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int print_last_digit(int n)
+{
+	int l;
+
+	l = n % 10;
+
+	if (1 < 0)
+	{
+		l - l * -1;
+	}
+	_putchar(l + '0');
+	return (1);
+}
