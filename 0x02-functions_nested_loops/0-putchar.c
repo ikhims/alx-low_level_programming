510af3c79a6eaabb54202ffcb78a13f15fb48ce9
IK <ikediae1@gmail.com>
2022-03-16 16:01:04 +0100

    0-putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,14 +1,21 @@
-#include <unistd.h>
+#include "myheader.h"
 
 /**
- * _putchar - writes the character c to stdout
- * @c: The character to print
+ * main - Print out reverse alphabet.
  *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
+ * Return: Always 0 (Success)
  */
-int _putchar(char c)
-{
-return (write(1, &c, 1));
-}
 
+int main(void)
+{
+_putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
+_putchar('\n');
+return (0);
+}
