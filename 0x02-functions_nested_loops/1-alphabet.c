4ead168a6575d47ea28acf4755b2d6080312a4e6
IK <ikediae1@gmail.com>
2022-03-17 00:49:10 +0100

    1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -2,21 +2,22 @@
 
 /**
  * print_alphabet - prints the lowercase alphabet
+ *
  * 
  * Return: void
  */
 
 void print_alphabet(void)
-[
+{
 	char c = 'a';
-	int i, j;
+	int i;
 
-	for (i=0; i < 10; i++)
+	for (i=0; i < 26; i++)
 {
 		for (j = 0; j < 26; j++)
 		{
-			_putchar(c + j);
+			_putchar(c + i);
 		}
 		_putchar(10);
 	}
-}
+i}
