02b0083e2f24ab948a5ccae133a50f02ca77364f
IK <ikediae1@gmail.com>
2022-03-28 22:36:46 +0100

    task 6 updated

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -1,32 +1,20 @@
 #include "main.h"
 
 /**
- * _strstr - function locate
- * @haystack: pointer to char
- * @needle: pointer to char
- * Return: 0
+ * print_chessboard - prints chessboard.
+ * @a: matrix
  */
 
-char *_strstr(char *haystack, char *needle)
+void print_chessboard(char (*a)[8])
 {
-char *result = haystack, *fneedle = needle;
+int i, j;
 
-while (*haystack)
+for (i = 0; i < 8; i++)
 {
-while (*needle)
+for (j = 0; j < 8; j++)
 {
-if (*haystack++ != *needle++)
-{
-break;
+_putchar(*(*(i + a) + j));
 }
+_putchar('\n');
 }
-if (!*needle)
-{
-return (result);
-}
-needle = fneedle;
-result++;
-haystack = result;
-}
-return (0);
 }
